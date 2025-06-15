1e686796b143ed90325989f224f971439949d199
Vili Auvinen <2207441@edu.karelia.fi>
2025-06-15 19:46:08 +0300

    tehtävä 1.1 step 1 DONE

--- tehtavat1/src/App.jsx
+++ tehtavat1/src/App.jsx
@@ -9,18 +9,54 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
+  );
+};
+
+const Header = (props) => {
+  return (
+    <div>
+      <p>Course: {props.course}</p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        Part: {props.part1}, exercises: {props.exercises1}
+      </p>
+      <p>
+        Part: {props.part2}, exercises: {props.exercises2}
+      </p>
+      <p>
+        Part: {props.part3}, exercises: {props.exercises3}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
   );
 };
 
