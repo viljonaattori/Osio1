7df8ca329778722c0aef8ac7fe570ada139cc8f0
Vili Auvinen <2207441@edu.karelia.fi>
2025-07-02 19:42:00 +0300

    tehtävät 1.6-1.14 tehty

--- tehtavat1/tehtavat1.6-1.14/src/App.jsx
+++ tehtavat1/tehtavat1.6-1.14/src/App.jsx
@@ -44,10 +44,35 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
 };
 
 const App = () => {
-  // tallenna napit omaan tilaansa
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(() => Array(anecdotes.length).fill(0));
+
+  const handleNextClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  const handleVoteClick = () => {
+    const updateVotes = [...votes]; // Kopio vanhan taulukon
+    updateVotes[selected] += 1; // Laskuri
+    setVotes(updateVotes); // Päivittää tilan
+  };
+
+  const maxVotes = Math.max(...votes); // Hakee suurimman äänimäärän saaneen anekdootin
+  const mostVotedIndex = votes.indexOf(maxVotes); // Selvittää indexin
 
   const handleGoodClick = () => {
     const updatedGood = good + 1;
@@ -69,8 +94,16 @@ const App = () => {
   const positive = (good / all) * 100 + " %";
   return (
     <div>
-      <Header />
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVoteClick}>Vote</button>
+      <button onClick={handleNextClick}>Next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVotedIndex]}</p>
+      <p>has {maxVotes} votes</p>
 
+      <Header />
       <Button onClick={handleGoodClick} text="Good" />
       <Button onClick={handleNeutralClick} text="Neutral" />
       <Button onClick={handleBadClick} text="Bad" />
